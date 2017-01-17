54e8edc72fdf2b488dedb2f448991dbc8c73afe2
Julien <julien.fournier@ynov.com>
2017-01-17 18:44:42 +0100

    HotFix guess_number

--- js/guess_number.js
+++ js/guess_number.js
@@ -16,11 +16,24 @@ function guess_number(){
     var milieu = localStorage.getItem('mid');
     console.log(milieu);
     if((max - min) > 1){
-    var question = "Est-ce que ton nombre est strictement supérieur à " + milieu + " ? (oui/non) ";
+    var question = "Est-ce que ton nombre est strictement supérieur à " + parseInt(milieu) + " ?";
     document.getElementById("question").innerHTML=question;
     }
     else{
-        console.log("end");
+        document.getElementById("oui").setAttribute('onclick',"ouiFin()");
+        console.log("Quasi fini");
+        question = "Votre nombre est-il : " + parseInt(max);
+        document.getElementById("question").innerHTML=question;
+        //if(){
+         //   question = "J'ai gagné ! C'était (" + parseInt(max) + ")";
+        //    document.getElementById("question").innerHTML=question;
+        //}
+        //else(){
+        //    question = "J'ai gagné ! C'était (" + parseInt(min) + ")";
+         //   document.getElementById("question").innerHTML=question;
+        //}
+        console.log("max = " + parseInt(max) + " min = " + parseInt(min));
+
     }
 }
 
@@ -28,10 +41,10 @@ function oui(){
     var max = localStorage.getItem('max');
     var min = localStorage.getItem('min');
     var mid = localStorage.getItem('mid');
-    min = mid;
+    min = parseInt(mid);
     localStorage.removeItem('mid');
-    mid = (max+min)/2;
-    console.log("dans oui");
+    var tot=parseInt(min)+parseInt(max);
+    mid = parseInt((tot)/2);
     localStorage.setItem('min',min);
     localStorage.setItem('mid',mid);
     guess_number();
@@ -40,11 +53,23 @@ function non(){
     var max = localStorage.getItem('max');
     var mid = localStorage.getItem('mid');
     var min = localStorage.getItem('min');
-    max=mid;
+    max=parseInt(mid);
     localStorage.removeItem('mid');
-    mid = (max+min)/2;
+    var tot=parseInt(min)+parseInt(max);
+    mid = parseInt((tot)/2);
     console.log("dans non");
     localStorage.setItem('max',max);
     localStorage.setItem('mid',mid);
     guess_number();
+}
+
+function ouiFin(){
+    question = "J'ai gagné ! ";
+    document.getElementById("question").innerHTML=question;
+    remove('bouttons');
+}
+function remove(id) {
+    var elem = document.getElementById(id);
+    elem.parentNode.removeChild(elem);
+    return false;
 }
