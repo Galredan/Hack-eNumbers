0ace7e1f606249d002e03fe6154d103bd8f0056f
Julien <julien.fournier@ynov.com>
2017-01-21 12:55:49 +0100

    Fix games

--- js/find_number.js
+++ js/find_number.js
@@ -1,16 +1,47 @@
-function find_number(){
-	var nombre = Math.floor(Math.random()*15) ;
-	var essai = 0 ;
-	console.log(nombre);
-	while (essai != nombre )
-	{
-		essai = prompt("Saissez votre nombre");
-		if (essai > nombre) {
-			alert("Plus bas");
-		}else if (essai < nombre){
-			alert("Plus haut");
-		}
-	}
-	alert('Bien joué !');
+function initFind() {
+    $('#findInput').removeClass('hidden');
+    $('#findStartButton').addClass('hidden');
+    $('#essais').removeClass('hidden');
+    $('#indice').removeClass('hidden');
+    console.log('debut du game');
+    var essais = 0;
+    var nombre = Math.floor(Math.random() * 15);
+    var proposition;
+    localStorage.setItem('essais', essais);
+    localStorage.setItem('proposition', proposition);
+    localStorage.setItem('toFind', nombre);
 }
-//find_number();
+
+function find_number() {
+    var nombre = localStorage.getItem('toFind');
+    var proposition = localStorage.getItem('proposition');
+    var essais = localStorage.getItem('essais');
+    if (proposition != nombre) {
+        console.log('echec');
+        essais++;
+        $('#nbEssais').html(essais);
+        localStorage.setItem('essais', essais);
+        // indice
+        if (parseInt(proposition) < parseInt(nombre)) {
+            console.log('plus');
+            $('#setHelp').html('plus');
+        } else {
+            console.log('moins');
+            $('#setHelp').html('moins');
+        }
+    } else {
+        $('#findInput').addClass('hidden');
+        $('#essais').removeClass('hidden');
+        $('#indice').addClass('hidden');
+        $('#initFind').addClass('hidden');
+        $('#winMessage').removeClass('hidden');
+        console.log('win');
+    }
+}
+
+function findPropose() {
+    var proposition = $('#proposition').val();
+    console.log(proposition);
+    localStorage.setItem('proposition', proposition);
+    find_number();
+}
--- js/guess_number.js
+++ js/guess_number.js
@@ -1,82 +1,86 @@
-
-function launchgame(){
+function launchgame() {
+    $('#exitButton').addClass('hidden');
+    $('#count').removeClass('hidden');
+    $('#init').removeClass('hidden');
+    $('#question').addClass('hidden');
+    $('#bouttons').addClass('hidden');
     var max = 100;
     var min = 0;
-    var mid = (max+min)/2;
-    localStorage.setItem('max',max);
-    localStorage.setItem('min',min);
-    localStorage.setItem('mid',mid);
+    var mid = (max + min) / 2;
+    localStorage.setItem('max', max);
+    localStorage.setItem('min', min);
+    localStorage.setItem('mid', mid);
     console.log("Lancement");
+    console.log('Initialisation des boutons');
+
+    var lancement = "Choisissez un nombre entre 0 et 100, je vais le deviner !";
+    $('#init').html(lancement);
+    setTimeout('$("#count").html(5)', 1000);
+    setTimeout('$("#count").html(4)', 2000);
+    setTimeout('$("#count").html(3)', 3000);
+    setTimeout('$("#count").html(2)', 4000);
+    setTimeout('$("#count").html(1)', 5000);
+    setTimeout('$("#count").html("GOOO")', 6000);
+    setTimeout('$("#startButton").removeClass("hidden")', 6000)
+}
+
+function initGuess() {
+    console.log("DEBUT DU JEUX");
+    $('#startButton').addClass('hidden');
+    $('#count').addClass('hidden');
+    $('#init').addClass('hidden');
+    $('#question').removeClass('hidden');
+    $('#bouttons').removeClass('hidden');
     guess_number();
 }
 
-function guess_number(){
+function guess_number() {
     var max = localStorage.getItem('max');
     var min = localStorage.getItem('min');
     var milieu = localStorage.getItem('mid');
-    console.log(milieu);
-    if((max - min) > 1){
-    var question = "Est-ce que ton nombre est strictement supérieur à " + parseInt(milieu) + " ?";
-    document.getElementById("question").innerHTML=question;
-    }
-    else{
-        document.getElementById("oui").setAttribute('onclick',"ouiFin()");
+    if ((max - min) > 1) {
+        var question = "Est-ce que ton nombre est strictement supérieur à " + parseInt(milieu) + " ?";
+        document.getElementById("question").innerHTML = question;
+    } else {
+        $('#ouiButton').off('click').on('click', ouiFin());
         console.log("Quasi fini");
-        question = "Votre nombre est-il : " + parseInt(max);
-        document.getElementById("question").innerHTML=question;
-        //if(){
-         //   question = "J'ai gagné ! C'était (" + parseInt(max) + ")";
-        //    document.getElementById("question").innerHTML=question;
-        //}
-        //else(){
-        //    question = "J'ai gagné ! C'était (" + parseInt(min) + ")";
-         //   document.getElementById("question").innerHTML=question;
-        //}
+        question = "Votre nombre est : " + parseInt(max);
+        document.getElementById("question").innerHTML = question;
         console.log("max = " + parseInt(max) + " min = " + parseInt(min));
 
     }
 }
 
-function oui(){
+function oui() {
     var max = localStorage.getItem('max');
     var min = localStorage.getItem('min');
     var mid = localStorage.getItem('mid');
     min = parseInt(mid);
     localStorage.removeItem('mid');
-    var tot=parseInt(min)+parseInt(max);
-    mid = parseInt((tot)/2);
-    localStorage.setItem('min',min);
-    localStorage.setItem('mid',mid);
+    var tot = parseInt(min) + parseInt(max);
+    mid = parseInt((tot) / 2);
+    localStorage.setItem('min', min);
+    localStorage.setItem('mid', mid);
     guess_number();
 }
-function non(){
+
+function non() {
     var max = localStorage.getItem('max');
     var mid = localStorage.getItem('mid');
     var min = localStorage.getItem('min');
-    max=parseInt(mid);
+    max = parseInt(mid);
     localStorage.removeItem('mid');
-    var tot=parseInt(min)+parseInt(max);
-    mid = parseInt((tot)/2);
+    var tot = parseInt(min) + parseInt(max);
+    mid = parseInt((tot) / 2);
     console.log("dans non");
-    localStorage.setItem('max',max);
-    localStorage.setItem('mid',mid);
+    localStorage.setItem('max', max);
+    localStorage.setItem('mid', mid);
     guess_number();
 }
 
-function ouiFin(){
+function ouiFin() {
     question = "J'ai gagné ! ";
-    document.getElementById("question").innerHTML=question;
-    remove('bouttons');
-    addExitButton();
+    $('#question').html(question);
+    $('#bouttons').addClass('hidden');
+    $('#exitButton').removeClass('hidden');
 }
-function remove(id) {
-    var elem = document.getElementById(id);
-    elem.parentNode.removeChild(elem);
-    return false;
-}
-
-function addExitButton(){
-    div = '<div><button type="button" class="btn btn-success" data-dismiss="modal">Revenir à la page d\'accueil</button></div>'
-    document.getElementById('fin-modal').innerHTML = div;
-}
-
